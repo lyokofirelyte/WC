2fff2e6aa3b5df476dab7bd1c6a073812fbad64b
Mark <snijderd@gmail.com>
2014-04-18 13:54:52 +0200

    something

--- common/com/github/lyokofirelyte/WC/Commands/WCKill.java
+++ common/com/github/lyokofirelyte/WC/Commands/WCKill.java
@@ -2,7 +2,6 @@ package com.github.lyokofirelyte.WC.Commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_7_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import com.github.lyokofirelyte.WC.WCMain;
@@ -48,7 +47,7 @@ public class WCKill {
 	}
 	
 	@WCLoop(time = 0, delay = 10, repeats = 5)
-	public void scream(CraftPlayer p){
+	public void scream(org.bukkit.craftbukkit.v1_7_R1.entity.CraftPlayer p){
 		
 		p.getWorld().playSound(p.getLocation(), Sound.ENDERMAN_STARE, 1.0F, 1.0F);
 		
